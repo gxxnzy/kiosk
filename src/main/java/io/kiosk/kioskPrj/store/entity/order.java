51974d9cad42fb4a4f7f0fa370ba808bee68cff6
DESKTOP-4U4G4G2\KOSA <koxn1974@naver.com>
2024-09-11 12:23:27 +0900

    작업중

--- src/main/java/io/kiosk/kioskPrj/store/entity/order.java
+++ src/main/java/io/kiosk/kioskPrj/store/entity/order.java
@@ -1,9 +1,6 @@
 package io.kiosk.kioskPrj.store.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,5 +10,12 @@ import lombok.Setter;
 public class order {
 
     @Id @Column(name = "order_name")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "store_name", nullable = false)
+    private String storeName; // 스토어 이름
+
+    @Column(name = "kiosks_num")
+    private Integer kiosksNum; // 키오스크 번호
 }
