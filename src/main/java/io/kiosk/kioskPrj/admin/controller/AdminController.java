4b5d9af465faf4ed0302c59de474931aa5957d16
deuk94 <knlu0025@gmail.com>
2024-09-11 12:38:46 +0900

    admin 페이지 전환 기능 구현

--- src/main/java/io/kiosk/kioskPrj/admin/controller/AdminController.java
+++ src/main/java/io/kiosk/kioskPrj/admin/controller/AdminController.java
@@ -1,5 +1,32 @@
 package io.kiosk.kioskPrj.admin.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("admin/")
 public class AdminController {
 
+    @GetMapping("adminMainForm")
+    public String adminMain() {
+        return "admin/adminMainForm";
+    }
+
+    @PostMapping("menuForm")
+    public String menuForm(Model model) {
+        return "admin/menuForm";
+    }
+
+    @PostMapping("storeForm")
+    public String storeForm() {
+        return "admin/storeForm";
+    }
+
+    @PostMapping("adForm")
+    public String adForm() {
+        return "admin/adForm";
+    }
 }
