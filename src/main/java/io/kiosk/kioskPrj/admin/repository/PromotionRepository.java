2ce6a3c8a51b69decafdfebe4991a298af3d82fa
SungJin Park <pk942@naver.com>
2024-09-21 02:26:15 +0900

    성진- 광고 생성,수정 구현

--- src/main/java/io/kiosk/kioskPrj/admin/repository/PromotionRepository.java
+++ src/main/java/io/kiosk/kioskPrj/admin/repository/PromotionRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.repository.query.Param;
 public interface PromotionRepository extends JpaRepository<Promotions, Integer> {
 
     @Query("SELECT p FROM Promotions p WHERE " +
-        "(:promotionName = '' OR p.promotionName LIKE %:promotionName%) AND " +
-        "(:promotionActive = -1 OR p.promotionActive = :promotionActive) ")
+        "(:promotionName = '' OR p.promotionName LIKE %:promotionName%) " +
+        "AND (:promotionActive = -1 OR p.promotionActive = :promotionActive)")
     List<Promotions> searchPromotions(String promotionName, int promotionActive, Sort sort);
 }
