acbe0ded00c0df23cbf12032bac9cac4536d9bde
YoungYoon <duddbs1994@naver.com>
2024-09-22 21:51:54 +0900

    코드 리팩토링

--- src/main/java/io/kiosk/kioskPrj/security/repository/UserRepository.java
+++ src/main/java/io/kiosk/kioskPrj/security/repository/UserRepository.java
@@ -4,7 +4,8 @@ import io.kiosk.kioskPrj.common.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 //@Repository 상속 없이 ioc 가능 JpaRepository 상속해서 자동 bean 등록
-public interface UserRepository extends JpaRepository<User,Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
+
     //select * from user where username = username
     public User findByUsername(String username);
 
